1fd05216fae3020cc22097039d9b66e999a67dd3
Richard Ly <rly758@gmail.com>
2024-11-04 17:26:27 -0800

    Added error status or message

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,5 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
 function ErrorPage() {
-  return <p>This is the ErrorPage component. </p>;
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
 }
 
 export default ErrorPage;
