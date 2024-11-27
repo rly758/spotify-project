a3cdc342bfe321cc8fcfc27f60d5c830bcf132fa
Richard Ly <rly758@gmail.com>
2024-11-27 01:23:05 -0800

    Added Loading animation

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,5 +1,17 @@
+import styles from "../styles/Loading.module.scss";
+
 function Loading() {
-  return <p>This is the Loading component. </p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.bars}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
