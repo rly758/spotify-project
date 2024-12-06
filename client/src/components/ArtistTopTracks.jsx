a43d39b83852d1e637d582b7b2a28b027b557f54
Richard Ly <rly758@gmail.com>
2024-12-06 09:03:46 -0800

    Added content

---
+++ client/src/components/ArtistTopTracks.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "./Loading";
+import TrackItem from "./TrackItem";
+import { getArtistTopTracks } from "../spotify";
+
+import styles from "../styles/ArtistTopTracks.module.scss";
+
+function ArtistTopTracks() {
+  const [topTracks, setTopTracks] = useState(null);
+  const { artistId } = useParams();
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function fetchData(artistId) {
+      const topTracks = await getArtistTopTracks(artistId);
+      console.log(topTracks);
+
+      if (!ignore) {
+        setTopTracks(topTracks);
+      }
+    }
+
+    try {
+      fetchData(artistId);
+    } catch (error) {
+      console.log(error);
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return topTracks ? (
+    <div className={styles.container}>
+      <h2>Top Tracks</h2>
+      <div className={styles.tracks}>
+        <ul>
+          {topTracks.tracks.map((track, i) => (
+            <TrackItem track={track} key={i} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+}
+
+export default ArtistTopTracks;
